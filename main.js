11a7930acb7e9f0ebc5ba9abdae61d3952abe293
Altify <Altify@mail.com>
2022-10-27 01:02:30 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+function process(event) {
+  var files = event.target.files
+  var reader = new FileReader()
+  reader.onload = function() {
+    var contents = this.result
+      console.log(contents);
+      let x = contents;
+      var code = x;
+            var i = 0;
+            document.addEventListener("keydown", function (){
+                terminal.innerText += code.slice(i, i+5);
+                i += 5;
+                if (i > code.length) {i=0;}
+            });
+            setInterval(function (){
+                var term = document.getElementById("terminal");
+                term.scrollTop = term.scrollHeight;
+            }, 100);
+}
+
+  reader.readAsText(files[0])
+}
+
+var input = document.querySelector('.file')
+input.addEventListener('change', process, false)
